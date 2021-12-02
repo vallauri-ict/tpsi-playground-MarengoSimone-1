af61b94319bbab5a51af5ac6062523a9c7e7898f
MarengoSimone <62547054+MarengoSimone@users.noreply.github.com>
2021-12-02 11:37:36 +0100

    Terminato esercizio 1

--- Express/esercizio1 Express/static/index.js
+++ Express/esercizio1 Express/static/index.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     $("#btnInvia").on("click", function() {
-        let request = inviaRichiesta("get", "/api/risorsa1", {"nome":"pippo"});
+        let request = inviaRichiesta("get", "/api/servizio1", {"nome":"Unico"});
         request.fail(errore);
         request.done(function(data) {
             alert(JSON.stringify(data));
@@ -9,10 +9,25 @@ $(document).ready(function() {
     });
 
     $("#btnInvia2").on("click", function() {
-        let request = inviaRichiesta("post", "/api/risorsa1", {"nome":"pluto"});
+        let request = inviaRichiesta("patch", "/api/servizio2", {"nome":"Unico","vampires":3});
         request.fail(errore);
         request.done(function(data) {
-            alert(JSON.stringify(data));
+            if(data.modifiedCount > 0)
+            {
+                alert("Aggiornamento eseguito correttamente");
+            }
+            else
+            {
+                alert("Nessuna corrispondenza trovata");
+            }
+        });
+    });
+
+    $("#btnInvia3").on("click", function() {
+        let request = inviaRichiesta("get", "/api/servizio3/m/brown");
+        request.fail(errore);
+        request.done(function(data) {
+            console.log(data);
         });
     });
 });
